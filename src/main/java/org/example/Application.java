2240f2a066e6b9ec398be2590ec35918814aea87
Ygor Garofalo <igorgarofalo@gmail.com>
2023-12-21 23:49:18 +0100

    fixes on main

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -14,10 +14,7 @@ public class Application
 {
     private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("Atac");
 
-    public static void main(String[] args)
-
-    {
-
+    public static void main(String[] args) {
         //  ****************************************DAO****************************************
         EntityManager em = emf.createEntityManager();
         AbbonamentoDAO ad = new AbbonamentoDAO(em);
@@ -31,31 +28,51 @@ public class Application
 
         //****************************************GENERATORS****************************************
         UserGenerator userGenerator = new UserGenerator();
-
         BigliettoGenerator ticketgenerator= new BigliettoGenerator();
         NegoziGenerator negoziGenerator = new NegoziGenerator();
         MezzoGenerator mezzoGenerator = new MezzoGenerator();
+        AbbonamentiGenerator abbonamentiGenerator = new AbbonamentiGenerator();
 
-        //****************************************METODI****************************************
+
+        //---------------------------CARICO IL DATABASE INIZIALE-------------------------------
+
+        System.out.println("-------GENERAZIONE UTENTI-----------");
         //userGenerator.getUsers();
+
+        Utente utente1fromdb = ud.findById(6);
+        Utente utente2fromdb = ud.findById(10);
+
+        System.out.println("-------GENERAZIONE RIVENDITORI-----------");
         //negoziGenerator.negozio();
+        Rivenditore rivenditorefromDb = rd.findById(16);
+
+        System.out.println("-------GENERAZIONE MEZZI-----------");
         //mezzoGenerator.generaMezzi();
 
-        Rivenditore negozio = rd.findById(17);
-        Utente utente1fromdb = ud.findById(46);
-
-        //ticketgenerator.getbiglietti(17);
-
-        //negoziGenerator.negozio();
-        //ud.rinnovoTessera(utente1fromdb);
-
-        //ud.generaTessera(utente1fromdb);
+        Mezzo bus = md.findById(26);
+        Mezzo tram = md.findById(36);
 
 
+        //----------------GENERO BIGLIETTI DOPO AVERE CARICATO SU DB DEI RIVENDITORI----------------
 
-    
+        System.out.println("--------GENERAZIONE DI BIGLIETTI DA UN NEGOZIO O DISTRIBUTORE SPECIFICO----------");
+        //ticketgenerator.getbiglietti(16);
+
+
+        //----------------GENERO ABBONAMENTI DOPO AVERE CARICATO SU DB DEGLI UTENTI----------------
+
+        System.out.println("-----GENERAZIONE DI ABBONAMENTI DATO UN UTENTE ED UN RIVENDITORE-----------");
+
 
 
 
     }
+
+
+
+
+
+
+
+
 }
